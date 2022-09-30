46dbfb67c393a1d1e59716fe5a14b9fba45f7347
Ameybairolu <ameybairolu@gmail.com>
2022-09-30 09:32:40 +0530

    Revamp experience section

--- src/features/experience/containers/index.tsx
+++ src/features/experience/containers/index.tsx
@@ -1,8 +1,55 @@
+import "./experience.css";
+
 import { ReactElement } from "react";
-import Experience from "../components/experience";
+
+import ExperienceDetails from "../components/experienceDetails";
+
+import SectionTitle from "commonComponents/sectionTitle";
 
 const ExperienceContainer = (): ReactElement<any, any> => {
-    return <Experience />;
+    const titleDetails = { minorTitle: "The Skills I Have", majorTitle: "My Experience" };
+
+    const primaryExperienceDetails = {
+        title: "Frontend Web Development",
+        experienceDetails: [
+            { title: "ReactJS", level: "Experienced" },
+            { title: "React Router DOM", level: "Experienced" },
+            { title: "Redux", level: "Experienced" },
+            { title: "HTML5", level: "Experienced" },
+            { title: "CSS3", level: "Intermediate" },
+            { title: "Bootstrap", level: "Intermediate" },
+            { title: "MUI", level: "Experienced" },
+            { title: "Tailwind", level: "Beginner" }
+        ]
+    };
+
+    const secondaryExperienceDetails = {
+        title: "Programming Languages & UI/UX Desiging",
+        experienceDetails: [
+            { title: "JavaScript", level: "Experienced" },
+            { title: "TypeScript", level: "Experienced" },
+            { title: "DSA in Java", level: "Experienced" },
+            { title: "Python", level: "Intermediate" },
+            { title: "C", level: "Intermediate" },
+            { title: "Figma", level: "Intermediate" }
+        ]
+    };
+
+    return (
+        <section id="experience">
+            <SectionTitle {...titleDetails} />
+            <div className="container experience__container">
+                <div className="experience__frontend">
+                    <h3>{primaryExperienceDetails.title}</h3>
+                    <ExperienceDetails experienceDetails={primaryExperienceDetails.experienceDetails} />
+                </div>
+                <div className="experience__backend">
+                    <h3>{secondaryExperienceDetails.title}</h3>
+                    <ExperienceDetails experienceDetails={secondaryExperienceDetails.experienceDetails} />
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default ExperienceContainer;
